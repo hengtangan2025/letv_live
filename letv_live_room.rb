648c90cf0140edcb79ce2f754b4e1eeaace50dc2
hengtangan2025 <504371515@qq.com>
2016-08-09 10:01:02 +0800

    获取推流地址和token

--- letv_live_room.rb
+++ letv_live_room.rb
@@ -55,7 +55,35 @@ class LetvLiveRoom
 
   # 模块二
   def get_obs_url_and_code
+    token_params = {
+      :method => "lecloud.cloudlive.activity.getPushToken",
+      :ver => '3.1',
+      :userid => @@userid,
+      :timestamp => Time.now.to_i * 1000,
+      :activityId => @activity_id,
+    }
+    token_params[:sign] = Digest::MD5.hexdigest(token_params.sort_by{|k,v| k}.map{|k,v| k.to_s + v.to_s}.join() + @@secretkey)
+    str = token_params.map{|k,v| "&" + k.to_s + "=" + v.to_s}.join()
+    str[0] = ""
+    uri = URI("http://api.open.letvcloud.com/live/execute" + "?" + str)
+    @pushToken = JSON.parse(Net::HTTP.get(uri))["token"]
+    p @pushToken
+    p "成功获取token"
 
+    push_url_params = {
+      :method => "lecloud.cloudlive.activity.getPushUrl",
+      :ver => '3.1',
+      :userid => @@userid,
+      :timestamp => Time.now.to_i * 1000,
+      :activityId => @activity_id,
+    }
+    push_url_params[:sign] = Digest::MD5.hexdigest(push_url_params.sort_by{|k,v| k}.map{|k,v| k.to_s + v.to_s}.join() + @@secretkey)
+    str = push_url_params.map{|k,v| "&" + k.to_s + "=" + v.to_s}.join()
+    str[0] = ""
+    uri = URI("http://api.open.letvcloud.com/live/execute" + "?" + str)
+    @pushUrl = JSON.parse(Net::HTTP.get(uri))
+    p @pushUrl["lives"][0]["pushUrl"]
+    p "成功获取推流地址"
   end
   # 模块三
 
@@ -65,14 +93,11 @@ class LetvLiveRoom
 
 end
 
-LetvLiveRoom.set_userid(823474)
-LetvLiveRoom.set_secretkey("f9e35f28944743e38ceccf9ab00364ab")
+LetvLiveRoom.set_userid(857550)
+LetvLiveRoom.set_secretkey("ed854df4e84e891fc838cb09be9a5039")
 
-# 模块一
+# # 模块一
 activity = LetvLiveRoom.new("hello world1", 30)
 
 # 模块二
-# puts activity.get_obs_url_and_code
-
-
-
+activity.get_obs_url_and_code
